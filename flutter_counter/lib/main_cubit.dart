8559b6933d89802ae99cf8c9db372f80759441f4
Samuel Adekunle <samuelbeebest@gmail.com>
2021-11-02 03:59:23 +0100

    updates

---
+++ flutter_counter/lib/main_cubit.dart
@@ -0,0 +1,15 @@
+import 'package:bloc/bloc.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+  void decrement() => emit(state - 1);
+}
+
+
+void main(List<String> args) {
+  final counter = CounterCubit();
+
+  counter.increment();
+}
