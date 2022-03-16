0f4a06ecc700363c05ea334c838286ef9869c3b9
Samuel Adekunle <samuelbeebest@gmail.com>
2022-03-16 16:00:21 +0100

    Bloc Architecture

--- bloc_counter/lib/main.dart
+++ bloc_counter/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:bloc_counter/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'business_logic/cubit/counter_cubit.dart';
 
 void main() {
   runApp(const MyApp());
